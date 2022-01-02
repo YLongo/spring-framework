cfe293d6cfa6c97c614070cbc7d884781ed1cf2b
YLongo <yhlona523@gmail.com>
2022-01-02 23:07:54 +0800

    init
    Signed-off-by: YLongo <yhlona523@gmail.com>

---
+++ spring-example/src/main/java/com/spring/Test.java
@@ -0,0 +1,15 @@
+package com.spring;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+//		context.register(Bean.class);
+		context.refresh();
+
+		System.out.println(context.getBean(Bean.class));
+	}
+}
